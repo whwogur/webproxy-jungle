da0bc41edf244b333400a32106ef65219afeafb1
whwogur <cjh.lux@gmail.com>
2023-04-17 05:27:34 +0000

    implement 11.10

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,37 +1,32 @@
-/*
- * adder.c - a minimal CGI program that adds two numbers together
- */
-/* $begin adder */
 #include "csapp.h"
 
-int main(void) {
-  char *buf, *p;
-  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
-  int n1 = 0, n2 = 0;
+int main(){
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1=0, n2=0;
 
-  /* Extract the twp arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL) {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p+1);
-    n1 = atoi(arg1);
-    n2 = atoi(arg2);
-  }
+    /* 인자 2개 추출하기 */
+    if ((buf = getenv("QUERY_STRING")) != NULL)
+    {
+        p = strchr(buf, '&');
+        *p = '\0';
+        sscanf(buf, "first=%d", &n1); // buf에서 %d를 읽어서 n1에 저장
+        sscanf(p+1, "second=%d", &n2);
+    }
 
-  /* Make the response body */
-  sprintf(content, "QUERY_STRING=%s", buf);
-  sprintf(content, "Welcome to add.com: ");
-  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
-  sprintf(content, "%sThanks for visiting!\r\n", content);
-  /* Generate the HTTP response */
-  printf("Connection: close\r\n");
-  printf("Content-length: %d\r\n", (int)strlen(content));
-  printf("Content-type: text/html\r\n\r\n");
-  printf("%s", content);
-  fflush(stdout);
+    /* 응답 본체 만들기 */
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+        content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
 
-  exit(0);
+    /* 클라이언트에 보낼 응답 출력하기 */
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+
+    exit(0);
 }
-/* $end adder */
