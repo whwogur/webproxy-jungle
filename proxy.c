5bea8e2ed42580c3157dda30f3e5e2219f7934e1
whwogur <cjh.lux@gmail.com>
2023-04-18 08:33:04 +0000

    seq proxy

--- proxy.c
+++ proxy.c
@@ -6,7 +6,7 @@
 #define MAX_OBJECT_SIZE 102400
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr ="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *user_agent_hdr ="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 112.0.0.0 Safari/537.36\r\n";
 static const char *conn_hdr = "Connection : close\r\n";
 static const char *prox_hdr = "Proxy-Connection : close\r\n";
 static const char *host_hdr_format = "Host : %s\r\n";
